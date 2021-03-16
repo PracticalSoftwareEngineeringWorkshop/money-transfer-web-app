11199a2733e39647412e6152fb99ce36a98ba76b
Biniam Asnake <biniam.asnake@kaufda.de>
2021-03-16 09:15:45 +0100

    Create Account form and API call implemented

--- src/components/CreateAccount/CreateAccount.js
+++ src/components/CreateAccount/CreateAccount.js
@@ -1,10 +1,103 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import axios from 'axios';
 
-const CreateAccount = () => {
+const CreateAccount = ({ history }) => {
+
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const [pin, setPin] = useState("")
+
+    const [submitForm, setSubmitForm] = useState(false);
+
+    function handleSubmit() {
+        setSubmitForm(true);
+    }
+
+    useEffect(() => {
+        if (submitForm === true) {
+            axios
+                .post('http://localhost:8080/api/account/create',
+                    {
+                        'firstName': firstName,
+                        'lastName': lastName,
+                        'email': email,
+                        'phoneNumber': phoneNumber,
+                        'pin': pin
+                    })
+                .then((response) => {
+                    const { data } = response
+                    console.log(data);
+                    alert(data.message);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    alert(error.error);
+                })
+            history.push('/')
+        }
+    }, [email, firstName, lastName, phoneNumber, pin, submitForm, history])
 
     return (
         <div>
-            <button>Create Account</button>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    type="text"
+                    placeholder="First Name"
+                    name="firstName"
+                    variant="outlined"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    required
+                    autoFocus
+                />
+
+                <TextField
+                    type="text"
+                    placeholder="Last Name"
+                    name="lastName"
+                    variant="outlined"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    required
+                />
+
+                <TextField
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    variant="outlined"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+
+                <TextField
+                    type="tel"
+                    placeholder="Phone Number"
+                    name="phoneNumber"
+                    variant="outlined"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    required
+                />
+
+                <TextField
+                    type="password"
+                    placeholder="Pin"
+                    name="pin"
+                    variant="outlined"
+                    value={pin}
+                    onChange={(e) => setPin(e.target.value)}
+                    required
+                />
+                <Button type="button" color="primary" onClick={(e) => handleSubmit()}>
+                    Create Account
+                </Button>
+            </form>
+
         </div>
     );
 }
