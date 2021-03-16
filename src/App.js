8f7c27c73b0b502c9c8265ebb09abcf9fdaad957
Biniam Asnake <biniam.asnake@kaufda.de>
2021-03-16 08:14:53 +0100

    React Router configured

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { CreateAccount, ListAccounts } from './components';
 
 const App = () => {
   // <> </> is a React Fragment
   return (
-    <>
-      <ListAccounts />
-      <CreateAccount />
-    </>
+    <Switch>
+      <Route
+        exact
+        path='/'
+        render={(props) => <ListAccounts {...props} />} />
+
+      <Route
+        exact
+        path='/create'
+        render={(props) => <CreateAccount {...props} />} />
+    </Switch>
   );
 }
 
