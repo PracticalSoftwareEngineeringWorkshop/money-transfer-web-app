f2386b74b83e45115aaca3bd5d9028aed4560281
Biniam Asnake <biniam.asnake@kaufda.de>
2021-03-16 13:00:05 +0100

    Renamed title

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.REACT_APP_MONEY_TRANSFER_API_BASE_URL
+export const API_BASE_URL = 'https://money-transfer-api-biniam.herokuapp.com/api';
+//process.env.REACT_APP_MONEY_TRANSFER_API_BASE_URL
