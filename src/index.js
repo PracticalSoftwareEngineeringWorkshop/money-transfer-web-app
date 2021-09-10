df53ef745a53c9800344e1991cd4c6788564502b
Biniam Asnake <biniamasnake@gmail.com>
2021-09-10 18:25:33 +0200

    Fixing router not working in GithubPages using https://www.freecodecamp.org/news/deploy-a-react-app-to-github-pages/

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
+    <HashRouter basename="/" history={history} >
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
